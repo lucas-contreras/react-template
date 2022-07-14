3400de0d45fd77a829ee9cc562ec2ac4ea1ed851
Noelia Arriola <noelia.arriola.1@gmail.com>
2022-07-14 19:51:40 -0300

    my first commit

--- src/App.js
+++ src/App.js
@@ -1,16 +1,44 @@
-import logo from './logo.svg';
+
 import './App.css';
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Fredoka:wght@600&display=swap');
+</style>
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+    <body>
+       <div className="portada">
+        <header className="App-header">
+        <img src="../logo.png" alt="logo"/>
+      <nav className='nav-bar'>
+			  <ul>
+			  	<li><a href="">Home</a></li>
+			  	<li><a href="">Quem Somos</a></li>
+			  	<li><a href="">Contato</a></li>
+		  	</ul>
+		  </nav>
       </header>
-      <p style={{color: "black"}}>Buenas tardes</p>
+      <p style={{color: "black"}}>INSTITUTO CONVENCER</p>
+        <h1>ENSINO DE QUALIDADE</h1>
+        <h3>Alunos que passam pelas nossas salas de aula saem
+        orientados para viver em qualquer esfera da
+        sociedade.</h3>
+        </div>
+
+<p>Instituto Convencer é formado por professores que acreditam
+na transformação individual de cada aluno. Todo nosso corpo docente tem a
+ vocação de ensinar, desempenhamos um papel de provocar no aluno a
+  instigação acadêmica, buscamos sempre ampliar as suas perspectivas.
+  Assumimos o papel central na formação do aluno, de modo ativo e criativo,
+  buscando sempre alcançar o seu maior potencial, superando suas expectativas. </p>
+
+<footer>
+  <nav>
+
+  </nav>
+</footer>
+</body>
     </div>
   );
 }
